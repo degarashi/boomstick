533ec6b2e29c21af774534b909dcd5287b5fe20f
degarashi <slice013@gmail.com>
2015-03-23 21:10:15 +0900

    AABB2D: Hit_Circle テストコード

--- tests/aabb2D_test.cpp
+++ tests/aabb2D_test.cpp
@@ -12,6 +12,7 @@ namespace boom {
 		using geo2d::AABB;
 		using geo2d::AABBM;
 		using geo2d::GSimplex;
+		using geo2d::CircleM;
 
 		class AABB2D : public spn::test::RandomTestInitializer {};
 		TEST_F(AABB2D, Hit_Point) {
@@ -43,6 +44,35 @@ namespace boom {
 			bool b0 = ab0.hit(ab1);
 			GSimplex gs(ab0, ab1);
 			bool b1 = gs.getResult();
+			ASSERT_EQ(b0, b1);
+		}
+		TEST_F(AABB2D, Hit_Circle) {
+			auto rd = getRand();
+			AABBM ab(GenRAABB(rd));
+			CircleM c(GenRCircle(rd));
+
+			bool b0 = false;
+			// 内包判定(Circle in AABB)
+			auto &amin = ab.minV,
+				&amax = ab.maxV;
+			if(c.support({1,0}).x <= amax.x
+				&& c.support({-1,0}).x >= amin.x
+				&& c.support({0,1}).y <= amax.y
+				&& c.support({0,-1}).y >= amin.y)
+				b0 = true;
+			// 内包判定(AABB in Circle)
+			if(ab.support({1,0}).x <= c.support({1,0}).x
+				&& ab.support({-1,0}).x >= c.support({-1,0}).x
+				&& ab.support({0,1}).y <= c.support({0,1}).y
+				&& ab.support({0,-1}).y >= c.support({0,-1}).y)
+				b0 = true;
+			// 4辺をセグメント判定
+			if(c.hit(SegmentM(amin, Vec2{amin.x, amax.y}))
+				|| c.hit(SegmentM(Vec2{amin.x, amax.y}, amax))
+				|| c.hit(SegmentM(amax, Vec2{amax.x, amin.y}))
+				|| c.hit(SegmentM(Vec2{amax.x, amin.y}, amin)))
+				b0 = true;
+			bool b1 = GSimplex(ab, c).getResult();
 			ASSERT_EQ(b0, b1);
 		}
 		TEST_F(AABB2D, Boundary) {
