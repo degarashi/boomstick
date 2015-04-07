9c3a9646120653aaf937650b195ac7f73bd0f883
degarashi <slice013@gmail.com>
2015-04-07 16:43:42 +0900

    EPA2DのテストコードをPoly, Circleに対応させた

--- tests/epa2D_test.cpp
+++ tests/epa2D_test.cpp
@@ -10,17 +10,24 @@ namespace boom {
 		using geo2d::GEpa;
 		template <class T>
 		class Epa2D : public spn::test::RandomTestInitializer {};
-		using Epa2DTypeList = ::testing::Types<geo2d::Circle,
+		using Epa2DTypeList = ::testing::Types<geo2d::Poly,
+												geo2d::Circle,
 												geo2d::AABB>;
 		TYPED_TEST_CASE(Epa2D, Epa2DTypeList);
 		namespace {
+			const spn::RangeF c_rangeV{-1e2f, 1e2f},
+								c_rangeR{0, 1e2f};
 			template <class RD>
 			void GenRShapeEPA(geo2d::CircleM& c, RD& rd) {
-				test2d::GenRShape(c, rd, spn::RangeF{-1e2f, 1e2f}, spn::RangeF{0, 1e2f});
+				test2d::GenRShape(c, rd, c_rangeV, c_rangeR);
+			}
+			template <class S, class RD>
+			void GenRShapeEPA(S& s, RD& rd) {
+				test2d::GenRShape(s, rd, c_rangeV);
 			}
 			template <class RD>
-			void GenRShapeEPA(geo2d::AABBM& a, RD& rd) {
-				test2d::GenRShape(a, rd, spn::RangeF{-1e2f, 1e2f});
+			void GenRShapeEPA(geo2d::ConvexM& c, RD& rd) {
+				test2d::GenRShape(c, rd, c_rangeV);
 			}
 		}
 
@@ -37,15 +44,24 @@ namespace boom {
 				auto pv = gepa.getPVector();
 				// 左辺に指定した物体を衝突回避ベクトル分移動させたら衝突回避出来る筈
 				c0 += pv;
-				c0.distend(-ErrorAdjust*2);
-				ASSERT_FALSE(GSimplex(c0, c1).getResult());
+				c0.distend(-ErrorAdjust);
+				GEpa gepa2(c0, c1, ErrorAdjust);
+				ASSERT_TRUE(!gepa2.getResult() ||
+							gepa2.getPVector().length() < ErrorAdjust);
 			} else {
 				auto np = gepa.getNearestPair();
 				// 左辺の物体を最近傍対ベクトル分(p1 - p0)移動させたら衝突する筈
 				auto pv = (np.second - np.first);
 				c0 += pv;
-				c0.distend(ErrorAdjust*2);
-				ASSERT_TRUE(GSimplex(c0, c1).getResult());
+				c0.distend(ErrorAdjust);
+				GEpa gepa2(c0, c1, ErrorAdjust);
+				bool b = gepa2.getResult();
+				if(!b) {
+					auto np2 = gepa2.getNearestPair();
+					float d2 = np2.first.distance(np2.second);
+					// 誤差の関係で割と範囲を広めにとる
+					ASSERT_LT(d2, 1e-1);
+				}
 			}
 		}
 	}
